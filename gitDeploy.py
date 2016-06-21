1fba6fbd89dc3c860e3bdffd92517a43ad38cdfd
jiangsir <jiangzero@gmail.com>
2016-06-21 16:45:40 +0800

    小修

--- gitDeploy.py
+++ gitDeploy.py
@@ -9,14 +9,14 @@ for file in os.listdir('/etc/init.d/'):
     if fnmatch.fnmatch(file, 'tomcat*'):
         tomcatN = file
         
-appname = input("請輸入 git host 上的應用程式名稱:")  # ex: ZeroBB
+appname = input("請輸入 git host 上的應用程式名稱: ")  # ex: ZeroBB
 apptmpdir = appname + "_" + datetime.datetime.now().strftime('%Y-%m-%d')
 
 os.system('rm -rf ' + apptmpdir)
 os.system('mkdir ' + apptmpdir)
 
 gituri = "ssh://git@git.nknush.kh.edu.tw:22/home/git/repository/" + appname + ".git " + apptmpdir;
-choose1 = input("預設 git uri=" + gituri + " (Y/n)")
+choose1 = input("預設 git uri=[" + gituri + "] (Y/n) ")
 if choose1 == "n":
     gituri = input("請輸入 git uri=")
 
@@ -34,9 +34,9 @@ for root, dirs, files in os.walk(apptmpdir + "/src/"):
              open(os.path.join(root, file), mode='w', encoding='utf-8').write(s)
 
 
-choose2 = input("使用原本的 app name=" + appname + " (Y/n)")
+choose2 = input("要發布的的 App Name=" + appname + " (Y/n) ")
 if choose2 == "n":
-    appname = input("請輸入要發布的的 appname= ")  # 如 ROOT
+    appname = input("請輸入要發布的的 appname=")  # 如 ROOT
 
 os.system('ant -f ' + apptmpdir + '/build.xml -Dappname=' + appname + ' -DTOMCAT_HOME=/usr/share/' + tomcatN + '/')
 
