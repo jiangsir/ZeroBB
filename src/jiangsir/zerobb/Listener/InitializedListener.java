e78708f0c43bb2e2abec422ae9a8fec1f2ef462e
jiangsir <jiangzero@gmail.com>
2016-06-27 16:05:33 +0800

    InitializedListener 加上 try catch

--- src/jiangsir/zerobb/Listener/InitializedListener.java
+++ src/jiangsir/zerobb/Listener/InitializedListener.java
@@ -18,79 +18,45 @@ public class InitializedListener implements ServletContextListener {
 	 *
 	 */
 	public void contextInitialized(ServletContextEvent event) {
-		// this.daemon = new Daemon();
-		// Thread daemonthread = new Thread(daemon);
-		// daemonthread.start();
-		// qx 暫不進行應用程式初始化<br>
-		// qx 可考慮在此將 properties.xml 全部讀入 Context Initialized
-		ServletContext context = event.getServletContext();
-		ApplicationScope.setAllAttributes(context);
+		try {
+			ServletContext context = event.getServletContext();
+			ApplicationScope.setAllAttributes(context);
 
-		Map<String, ? extends ServletRegistration> registrations = context
-				.getServletRegistrations();
-		for (String key : registrations.keySet()) {
-			String servletClassName = registrations.get(key).getClassName();
-			WebServlet webServlet;
-			try {
-				if (Class.forName(servletClassName).newInstance() instanceof HttpServlet) {
-					HttpServlet httpServlet = (HttpServlet) Class.forName(
-							servletClassName).newInstance();
-					webServlet = httpServlet.getClass().getAnnotation(
-							WebServlet.class);
-					if (webServlet != null) {
-						for (String urlpattern : webServlet.urlPatterns()) {
-							ApplicationScope.getUrlpatterns().put(urlpattern,
-									httpServlet);
+			Map<String, ? extends ServletRegistration> registrations = context.getServletRegistrations();
+			for (String key : registrations.keySet()) {
+				String servletClassName = registrations.get(key).getClassName();
+				WebServlet webServlet;
+				try {
+					if (Class.forName(servletClassName).newInstance() instanceof HttpServlet) {
+						HttpServlet httpServlet = (HttpServlet) Class.forName(servletClassName).newInstance();
+						webServlet = httpServlet.getClass().getAnnotation(WebServlet.class);
+						if (webServlet != null) {
+							for (String urlpattern : webServlet.urlPatterns()) {
+								ApplicationScope.getUrlpatterns().put(urlpattern, httpServlet);
+							}
 						}
 					}
+				} catch (InstantiationException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
 				}
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
 			}
+			for (String urlpattern : ApplicationScope.getUrlpatterns().keySet()) {
+				System.out.println(
+						"urlpattern=" + urlpattern + ", servlet=" + ApplicationScope.getUrlpatterns().get(urlpattern));
+			}
+			context.setAttribute("urlpatterns", ApplicationScope.getUrlpatterns());
+
+			ENV.context = context;
+			ENV.LastContextRestart = new Date();
+			ENV.setAPP_REAL_PATH(context.getRealPath("/"));
+			ENV.setAPP_NAME(context.getContextPath());
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		for (String urlpattern : ApplicationScope.getUrlpatterns().keySet()) {
-			System.out.println("urlpattern=" + urlpattern + ", servlet="
-					+ ApplicationScope.getUrlpatterns().get(urlpattern));
-		}
-		context.setAttribute("urlpatterns", ApplicationScope.getUrlpatterns());
-
-		ENV.context = context;
-		ENV.LastContextRestart = new Date();
-		ENV.setAPP_REAL_PATH(context.getRealPath("/"));
-		ENV.setAPP_NAME(context.getContextPath());
-		// ENV.setMyPropertiesPath(ENV.APP_REAL_PATH +
-		// "META-INF/properties.xml");
-		// ENV.resource = ResourceBundle.getBundle("resource");
-		// context.setAttribute("OnlineUsers",
-		// new Hashtable<String, HttpSession>());
-		// new GeneralDAO().getConnection();
-
-		// ENV.divisionmap.put("jiaowu", "教務處");
-		// ENV.divisionmap.put("xuewu", "學務處");
-		// ENV.divisionmap.put("zongwu", "總務處");
-		// ENV.divisionmap.put("fudao", "輔導室");
-		// ENV.divisionmap.put("renshi", "人事室");
-		// ENV.divisionmap.put("kuaiji", "會計室");
-		// ENV.divisionmap.put("lib", "圖資中心");
-		// ENV.divisionmap.put("zhucezu", "註冊組");
-		// ENV.divisionmap.put("shebei", "設備組");
-		// ENV.divisionmap.put("jiankang", "健康中心");
-		// ENV.divisionmap.put("documentation", "公文公告區");
-		// ENV.divisionmap.put("zixun", "資訊組");
-		// ENV.divisionmap.put("jiaoxue", "教學組");
-		// ENV.divisionmap.put("weisheng", "學務處衛保組");
-		// ENV.divisionmap.put("teachers", "教師研習");
-		// ENV.divisionmap.put("xuewuhonor", "學務處榮譽榜");
-		// ENV.divisionmap.put("principal", "校長室");
-		// ENV.divisionmap.put("schoolnews", "校園新聞");
-		// ENV.divisionmap.put("honoredlist", "榮譽榜");
-		// ENV.divisionmap.put("alumni", "校友會");
-		// ENV.divisionmap.put("shebei", "設備組");
-
 	}
 
 	public void contextDestroyed(ServletContextEvent event) {
